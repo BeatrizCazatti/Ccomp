1f2d1e00a5c7a0f8b9cb54b193e5830799894fd0
Beatriz <beatrizcazatti@gmail.com>
2024-11-04 13:39:05 -0300

    add exercício

--- Fundamentos da Computação/lista 01/12-.py
+++ Fundamentos da Computação/lista 01/12-.py
@@ -1,1 +1,8 @@
 #12: Crie um programa para calcular a área de um triângulo qualquer, considerando que são fornecidos os comprimentos dos seus lados. Esse programa não pode permitir a entrada de dados inválidos, ou seja, medidas menores ou iguais a 0.
+
+ladoA = int(input('Lado A: '))
+ladoB = int(input('Lado B: '))
+ladoC = int(input('Lado C: '))
+
+if ladoA <= 0 or ladoB <= 0 or ladoC <= 0:
+    print('Digite um valores válidos!')
