73acae9b1a82e4fd6ed3096b4ed61758735ba53e
Beatriz <beatrizcazatti@gmail.com>
2024-11-22 09:50:39 -0300

    add exercicio

--- Fundamentos da Computação/lista 01/02-PA.py
+++ Fundamentos da Computação/lista 01/02-PA.py
@@ -1,1 +1,8 @@
 #02: Crie um programa que escreva os N primeiros termos de uma progressão aritmética (PA). O primeiro termo e a razão da PA devem ser informados pelo usuário.
+
+a1 = int(input('a1: '))
+r = int(input('r: '))
+n = int(input('n: '))
+
+for ai in range(a1, n+1, r):
+    print(ai)
