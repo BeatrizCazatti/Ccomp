a28fb02b109898fced8cd537544e10ab43c78242
Beatriz <beatrizcazatti@gmail.com>
2024-11-22 09:43:36 -0300

    add exercicio

--- Fundamentos da Computação/lista 01/01-prefeitura.py
+++ Fundamentos da Computação/lista 01/01-prefeitura.py
@@ -2,6 +2,29 @@
     #a) a média salarial dos homens, a média salarial das mulheres;
     #b) o maior salário encontrado entre as pessoas abaixo de 30 anos.
     #Obs.: O final da leitura de dados é marcado por uma idade negativa.
+somaHomens = 0
+somaMulheres = 0
+numHomens = 0
+numMulheres = 0
+maiorSalario = 0
 
-sexo = str(input('Sexo: '))
-idade = int(input('Salário: '))
+while True:
+    sexo = str(input('Sexo: '))
+    idade = int(input('Idade: '))
+    if idade < 0:
+        break  
+    salario = int(input('Salário: '))
+    print(20*'=')
+    if sexo.upper() == 'F':
+        somaMulheres += salario
+        numMulheres += 1
+    if sexo.upper() == 'M':
+        somaHomens += salario
+        numHomens += 1
+    if idade < 30:
+        if salario > maiorSalario:
+            maiorSalario = salario
+        
+print(f'Média de salário entre as mulheres é {somaMulheres/numMulheres}')
+print(f'Média de salário entre os homens é {somaHomens/numHomens}')
+print(f'O maio salário foi {maiorSalario}')
