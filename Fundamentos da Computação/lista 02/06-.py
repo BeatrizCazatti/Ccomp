635d295185e540ba058e81080269314ea7db0d37
Beatriz <beatrizcazatti@gmail.com>
2024-12-26 13:02:07 -0300

    add

--- Fundamentos da Computação/lista 02/06-.py
+++ Fundamentos da Computação/lista 02/06-.py
@@ -1,1 +1,3 @@
 #06: Um palíndromo é uma cadeia que pode ser lida de frente para trás e de trás para frente. Exemplos: ‘SOMOS’, ‘1234321’. Implemente a função palindromo(palavra). O parâmetro palavra é uma string. A função deverá retornar True se for um palíndromo e False caso contrário.
+
+palavra = ""
