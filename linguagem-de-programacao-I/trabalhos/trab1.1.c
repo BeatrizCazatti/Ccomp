411fa0204fedd175dd2c79e11f7b2090f22addf8
Beatriz <beatrizcazatti@gmail.com>
2025-05-01 16:29:06 -0300

    add exercicio

--- linguagem-de-programacao-I/trabalhos/trab1.1.c
+++ linguagem-de-programacao-I/trabalhos/trab1.1.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 
 int main () {
-    int dia, mes, ano;
-    int diasNoMes[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    diasNoMes[1] = 29;
-    printf("%d", diasNoMes);
+    int dia, mes, ano, diasAno = 0;
+    int diasMes[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     scanf("%d %d %d", &dia, &mes, &ano);
+
+    if((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)){
+        diasMes[1] = 29;
+    }
+
+    for(int i = 0; i < mes-1; i++){
+        printf("%d\n", diasMes[i]);
+        diasAno += diasMes[i];
+    }
+
+    printf("%d", (diasAno + dia));
+
+    return 0;
 }
