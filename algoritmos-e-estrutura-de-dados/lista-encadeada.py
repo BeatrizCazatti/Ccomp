c0a36335e751dad85e7b5728ebd9e933302c04d3
Beatriz Cazatti Figueiredo <87504331+BeatrizCazatti@users.noreply.github.com>
2025-05-14 20:23:42 -0300

    Criar lista-encadeada.py

---
+++ algoritmos-e-estrutura-de-dados/lista-encadeada.py
@@ -0,0 +1,26 @@
+
+class No:
+
+def_init_(self, c, dados=None, prox=None):
+
+self.dados dados
+
+self.c = c
+
+self.prox = prox
+
+print(11 esbic)
+
+pront (11.cab dades)
+
+class ListaEncadeada:
+
+def_init__(self):
+
+self.cab = None
+
+11 Lista Encadeada()
+
+ll.cab = No 1, ['rótulo1': 'dados1', 'rótulo2', 'dados2'), None
+
+ll.cab.prox= No 2,['rótulo1': 'dados3', 'rótulo2', 'dados4'),
