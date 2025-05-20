48f23b44fe10b3d182e4dd4f0c15cfe6bf50b2cf
Beatriz <beatrizcazatti@gmail.com>
2025-05-19 21:18:53 -0300

    add exercicio

--- linguagem-de-programacao-I/exercicios/04-verificacao numero primo.c
+++ linguagem-de-programacao-I/exercicios/04-verificacao numero primo.c
@@ -7,3 +7,30 @@ Saída: 7 primo
 
 Entrada: 9
 Saída: 9 nao primo*/
+
+#include <stdio.h>
+
+int eh_primo (int num) {
+    if(num <= 1){
+        return 0;
+    } 
+    for(int i = 2; i < num; i++){
+        if(num % i == 0){ //tem divisor ou 0 ou 1 => nao eh primo, retorna falso(0)
+            return 0;
+        }
+    }
+    return 1;    
+}
+
+int main () {
+    int num;
+
+    scanf("%d", &num);
+    if(eh_primo(num)){
+        printf("%d primo", num);
+    }else{
+        printf("%d nao primo", num);
+    }
+
+    return 0;
+}
