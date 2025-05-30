90146a41f2b2b9e465f6feb79f2e80629fb84ee4
Beatriz <beatrizcazatti@gmail.com>
2025-05-30 13:16:36 -0300

    ajustes

--- algebra-linear/gram_shcmdt.py
+++ algebra-linear/gram_shcmdt.py
@@ -1,5 +1,4 @@
-
-#funções úteis
+#Funções úteis:
 def produto_escalar(v1, v2):
     resultado = 0
     for i in range(len(v1)):
@@ -22,22 +21,8 @@ def projecao_vetores(v1, v2):
     escalar = produto_escalar(v1, v2) / produto_escalar(v2, v2)
     return produto_vetor_por_escalar(v2, escalar)
 
-#Aplica Algopritmo de Gram-Shcmidt
-def ortogonalizar_base (base):
-    base_ortogonal = []
-    for i in range(len(base)):
-        vetor_ortogonal = base[i][:]
-        for j in range(len(base_ortogonal)):
-            proj = projecao_vetores(base[i], base_ortogonal[j])
-            vetor_ortogonal = subtracao_vetores(vetor_ortogonal, proj)
 
-        base_ortogonal.append(vetor_ortogonal)
-    return base_ortogonal
-
-
-
-
-#Input do usuário
+#Input do usuário:
 num_vetores = int(input('Digite o número de vetores da base: '))
 base = []
 
@@ -50,10 +35,21 @@ for n in range(1, num_vetores+1):
         componentes.append(float(s))
     base.append(componentes)
 
+
+#Aplicação do algopritmo de Gram-Shcmidt:
+def ortogonalizar_base (base):
+    base_ortogonal = []
+    for i in range(len(base)):
+        vetor_ortogonal = base[i][:]
+        for j in range(len(base_ortogonal)):
+            proj = projecao_vetores(base[i], base_ortogonal[j])
+            vetor_ortogonal = subtracao_vetores(vetor_ortogonal, proj)
+
+        base_ortogonal.append(vetor_ortogonal)
+    return base_ortogonal
+
+
+#Output:
 print("\nBase ortogonal:")
 for i, vetor in enumerate(ortogonalizar_base(base), 1):
     print(f"u{i} = {vetor}")
-
-
-
-#def normaliza_base
