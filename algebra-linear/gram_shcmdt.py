345d23d1ab5b44a47b580a64114e30831ffa2bb1
Beatriz <beatrizcazatti@gmail.com>
2025-05-31 20:31:50 -0300

    add exercicio

--- algebra-linear/gram_shcmdt.py
+++ algebra-linear/gram_shcmdt.py
@@ -21,7 +21,6 @@ def projecao_vetores(v1, v2):
     escalar = produto_escalar(v1, v2) / produto_escalar(v2, v2)
     return produto_vetor_por_escalar(v2, escalar)
 
-
 #Input do usuário:
 num_vetores = int(input('Digite o número de vetores da base: '))
 base = []
@@ -35,7 +34,6 @@ for n in range(1, num_vetores+1):
         componentes.append(float(s))
     base.append(componentes)
 
-
 #Aplicação do algopritmo de Gram-Shcmidt:
 def ortogonalizar_base (base):
     base_ortogonal = []
@@ -48,8 +46,8 @@ def ortogonalizar_base (base):
         base_ortogonal.append(vetor_ortogonal)
     return base_ortogonal
 
-
-#Output:
+#Output(formatado com aproximação de duas casas):
 print("\nBase ortogonal:")
 for i, vetor in enumerate(ortogonalizar_base(base), 1):
-    print(f"u{i} = {vetor}")
+    print(f"u{i} = [", end="")
+    print(", ".join(f"{x:.2f}" for x in vetor), end="]\n")
