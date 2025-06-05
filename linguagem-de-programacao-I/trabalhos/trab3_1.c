eed4464728594e4fa1a530a86f84ad7e6300f917
Beatriz <beatrizcazatti@gmail.com>
2025-06-05 12:29:26 -0300

    add exercicio

---
+++ linguagem-de-programacao-I/trabalhos/trab3_1.c
@@ -0,0 +1,8 @@
+/*Escreva um programa que retorne o número de palavras que contém uma frase, com no máximo 1024 caracteres, fornecida pelo canal padrão. Considere que qualquer separação por espaço, ponto, vírgula, ponto e vírgula, dois pontos, enter ou tab  termina uma palavra. Também considere que palavras com hífen como só uma palavra.
+Nesta versão, você deve usar o comando switch e implementar sua própria máquina de estados. Use como referência o diagrama de estados a seguir. Nele, os círculos representam os estados e as setas representam a mobilidade de estados em função do caractere lido.(.\maquina_estado.png)
+
+Ex: Vou ao mercado.
+Resultado: 3
+
+Ex: É preciso tomar o anti-inflamatório.
+Resultado: 5*/
---
+++ linguagem-de-programacao-I/trabalhos/trab3_2.c
@@ -0,0 +1,57 @@
+/*Faça um programa que leia uma data em formato texto ou numérico e a imprima na tela em formato numérico.
+
+As datas devem ser escritas em formato numérico tal qual utilizado na língua portuguesa, DD/MM/AAAA (dois caracteres para o dia, dois caracteres para o mês e quatro caracteres para o ano). Os números de dia e mês inferiores a 10 devem ser acrescidos de um algarismo zero a esquerda.
+
+Exemplo: 
+
+Entrada:
+1 de fevereiro de 2023
+Saída:
+01/02/2023
+
+Entrada:
+1/12/2023
+Saída:
+01/12/2023*/
+
+#include <stdio.h>
+#include <string.h>
+
+void data_texto_para_num (int day, char monthname[], int year) {
+    const char *meses[] = {"jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"};
+    
+    for(int i = 0; i < 12; i++) {
+        if(strncmp(monthname, meses[i], 3) == 0){
+            printf("%02d/%02d/%d", day, i+1, year);
+            break;
+        }
+    }
+}
+
+int main(void){
+
+    int month, day, year; 
+    char monthname[16], line[256];
+    char word1[16], word2[16], word3[16];
+
+    while (fgets(line, sizeof(line), stdin) > 0) { 
+        if (sscanf(line, "%d %s %d", &day, monthname, &year)== 3){ /* 25 Dec 1988 form */
+            printf("monthname:%s\n", monthname);
+            data_texto_para_num(day, monthname, year); 
+        } else if (sscanf(line, "%d/%d/%d", &month, &day, &year) == 3) {
+            printf("%02d/%02d/%d", day, month, year); /* mm/dd/yy form */ 
+        } else if (sscanf(line, "%d %s %s %s %d", &day, word1, word2, word3, &year) == 5) { /*11 de novembro 2005*/
+            if(strcmp(word1, "de")){
+                 printf("é um de");
+                 monthname = strcpy(word2);
+             }
+            printf("%d", day);
+            printf("%s", word1);
+            printf("%s", word2);
+            printf("%s", word3);
+            printf("%d", year);
+        }
+        else printf("invalid: %s", line); /* invalid form */ 
+    } 
+    return 0;
+}
