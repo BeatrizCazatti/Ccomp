40d7de5fb6cceadd592f3090dfe2ece89491438d
Beatriz <beatrizcazatti@gmail.com>
2025-07-12 12:19:28 -0300

    add exercicio

--- linguagem-de-programacao-I/programas_slides/fibonacci_recursivo.c
+++ linguagem-de-programacao-I/programas_slides/fibonacci_recursivo.c
@@ -13,10 +13,11 @@ int fibo_b(int n, int *ptrchamadas);
 
 int main (){
     int chamadas[5];
-    for(int i = 0; i < 5; i++) chamadas[i] = 0;
+    for(int i = 0; i < 5; i++) *(chamadas + i) = 0;
+    
     printf("%d\n", fibo_a(4));
     printf("%d", fibo_b(4, chamadas));
-    for(int i = 0; i < 5; i++) printf("\nchamadas_f%d: %d", i, chamadas[i]);
+    for(int i = 0; i < 5; i++) printf("\nchamadas_f%d: %d", i, (*chamadas)+i);
     return 0;
 }
 
