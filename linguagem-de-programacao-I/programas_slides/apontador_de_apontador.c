fdbb8f5b51a9281a0793c34db00881fc8fe16c44
Beatriz <beatrizcazatti@gmail.com>
2025-07-13 08:46:07 -0300

    add exercicio

--- linguagem-de-programacao-I/programas_slides/apontador_de_apontador.c
+++ linguagem-de-programacao-I/programas_slides/apontador_de_apontador.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+
+/*
+Apontador Duplo em C é usado principalmente para:
+- Modificar ponteiros dentro de funções (passar o endereço do ponteiro)
+- Matrizes dinâmicas
+*/
 
 int main () {
-    char* StringsArray[10];
-    int* IntPointerArray [25];
-    struct Estudante{
-        char nome[255];
-        char ID [12];
+    struct coord {
+        short x, y;
     };
-    //struct Estudante* Turma[15];
+    
+    //array dinamico de structs
+    struct coord *clist;
 
-    Estudante **turma = malloc(15 * sizeof(Estudante*)) //ponteiro para um array de ponteiros (um ponteiro de ponteiros).
-    //turma[i]: ponteiro para um estudante individual.
-
-    scanf("%[^\n]", &Turma[0]->nome); //lê até o \n: %[ ] → leia uma sequência de caracteres e ^ significa “exceto”,
-
-    printf("%s", Turma[0]->nome);
+    clist = malloc(2 * sizeof(*clist));
+    //clist[5] = {struct coord, struct coord}
+    (*(clist+1)).x = 2;
+    clist[1].y = 3;
+    printf("%hd\n", clist[1].x);
+    printf("%hd", (clist+1)->y);
 }
