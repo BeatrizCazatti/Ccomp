42415a1c4e1d3898fc152e3546b57233b96e143f
Beatriz <beatrizcazatti@gmail.com>
2025-08-21 10:32:17 -0300

    add exercicio

---
+++ lp-II/src/Exercicio04.java
@@ -0,0 +1,12 @@
+public class Exercicio04 {
+    public static void main(String[] args){
+        System.out.println(5 * 4 / 6 + 7);
+        System.out.println(5 * 4 / (6 + 7));
+        System.out.println(5 * 4.0 / 6 + 7);
+        System.out.println(5 * 4 % 6 + 7);
+        System.out.println(5 * 4 / (float)6 + 7);
+        System.out.println((4 / 3) + (3.0 * 5));
+        System.out.println((4 / 3.0) + (3 * 5));
+        System.out.println((int)(4 / 3.0) + (3 * 5));
+    }
+}
