c14b75951b900b63be51eef5d1496537678db5e5
Beatriz <beatrizcazatti@gmail.com>
2025-08-28 21:18:44 -0300

    pequenos ajustes

--- aed-II/tromino.py
+++ aed-II/tromino.py
@@ -1,8 +1,12 @@
-import numpy as np
+
+print('n: ', end='')
 n = int(input())
+print('x: ', end='')
 x = int(input())
+print('y: ', end='')
 y = int(input())
 m = []
+qtd = 1
 
 #matriz colorida           
 # for i in range(1, n+1):
@@ -19,41 +23,58 @@ def cria_matriz(m, n):
     for i in range(1, n+1):
         linha = []
         for j in range(1, n+1):
-            if(i == x and j == y):
+            if(j == x and i == y):
                 linha.append(1)
             else:
                 linha.append(0)
         m.append(linha)
 
-cria_matriz(m, n)
-
-#imprime matriz 
 def imprime_matriz(m, n):   
     for i in range(n):
         print(m[i])
     
-imprime_matriz(m, n)
-    
-def tromino(m, n, x, y):
-    mid = int(n / 2)
-    print(f'mid={mid}')
-    if(x <= mid and y > mid):
-        print('1Q')
-        m[mid][mid] = 2
-    if(x <= mid and y <= mid):
-        print('2Q')
-        m[mid][mid] = 2
-    if(x > mid and y <= mid):
-        print('3Q')
-        m[mid][mid] = 2
-        
-    if(x > mid and y > mid):
-        print('4Q')
-        m[mid][mid-1] = 2
-        m[mid-1][mid-1] = 2
-        m[mid-1][mid] = 2
+cria_matriz(m, n)
 
-tromino(m, n, x, y)
+#imprime matriz 
+#imprime_matriz(m, n)
+
+#r e c são as coordenadas do canto superior esquerdo do quadrado
+def tromino(m, n, x, y, qtd):
+    if(n > 1):
+        qtd += 1
+        mid = int(n / 2)
+        print(f'mid={mid}')
+        if(y <= mid and x > mid):
+            print('1Q')
+            m[mid][mid] = 1
+            m[mid][mid-1] = 2
+            m[mid-1][mid-1] = 3
+            #tromino(m, mid, mid-1, mid-1, qtd)
+        if(y <= mid and x <= mid):
+            print('2Q')
+            m[mid][mid] = 1
+            m[mid][mid-1] = 2
+            m[mid-1][mid] = 3
+        if(y > mid and x <= mid):
+            print('3Q')
+            m[mid][mid] = 1
+            m[mid-1][mid-1] = 2
+            m[mid-1][mid] = 3
+        if(y > mid and x > mid):
+            print('4Q')
+            m[mid][mid-1] = 1
+            m[mid-1][mid-1] = 2
+            m[mid-1][mid] = 3
+        #percorrer 1Q, 2Q, 3Q, 4Q
+        for i in range(n):
+            for j in range(n):
+                int 
+                m[r+i][c+j]
+
+        
+
+tromino(m, n, x, y, qtd)
 imprime_matriz(m, n)
+print(m[1][2])
 #def colocar_tromino(mid):
     
