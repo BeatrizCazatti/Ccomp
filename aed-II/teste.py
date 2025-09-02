5b6fcede2f36468c31b6ddec0f7403fdc64e0f25
Beatriz <beatrizcazatti@gmail.com>
2025-09-02 09:19:53 -0300

    add exercicio

--- aed-II/teste.py
+++ aed-II/teste.py
@@ -1,6 +1,5 @@
 N = int(input())
 
-#Ordenação
 def particione(notas, p, r):
     x = notas[r]
     i = p - 1
@@ -9,24 +8,20 @@ def particione(notas, p, r):
             i += 1
             notas[i], notas[j] = notas[j], notas[i]
     notas[i + 1], notas[r] = notas[r], notas[i + 1]
-    
     return i + 1
 
 def quick_sort(notas, notasOrig, p, r, numIguais):
     if(r > p):
         q = particione(notas, p, r)
-        print(f"q: {q}. {notas} e {notasOrig}")
-        if notas[q] == notasOrig[-q - 1]: numIguais += 1
-        quick_sort(notasOrig, notas, q + 1, r, numIguais)
-        quick_sort(notasOrig, notas, p, q - 1, numIguais)
-
+        quick_sort(notas, notasOrig, q + 1, r, numIguais)
+        quick_sort(notas, notasOrig, p, q - 1, numIguais)
+        for q in range(len(notas)): 
+          if notas[q] == notasOrig[-q - 1]: numIguais += 1
     return numIguais
 
 for i in range(N):
     M = int(input())
-    notas = [int(nota) for nota in input().split(" ")]
-
-    print(notas)
+    notas = [int(nota) for nota in input().split()]
     numIguais = quick_sort(notas, notas[:], 0, len(notas)-1, 0)
     print(numIguais)
-    print(notas)
+
