d0613ad366f9090b987ddea4a57bc21a4a22967b
Beatriz <beatrizcazatti@gmail.com>
2025-09-02 09:21:23 -0300

    add exercicio

--- aed-II/fila-recreio.py
+++ aed-II/fila-recreio.py
@@ -1,38 +1,27 @@
 N = int(input())
-filaOriginal = []
 
-for i in range(N):
-    somaNotas = 0 
-    M = int(input())
-    notas = [int(nota) for nota in input().split(" ")]
-    filaOriginal.append(notas)
-
-#Ordenação
-def particione(fila, p, r):
-    x = fila[r]
+def particione(notas, p, r):
+    x = notas[r]
     i = p - 1
     for j in range(p, r):
-        if(fila[j] <= x):
+        if(notas[j] <= x):
             i += 1
-            fila[i], fila[j] = fila[j], fila[i]
-        fila[i+1], fila[r] = fila[r], fila[i+1]
-        return i + 1
-    
-def quick_sort(fila, p, r):
+            notas[i], notas[j] = notas[j], notas[i]
+    notas[i + 1], notas[r] = notas[r], notas[i + 1]
+    return i + 1
+
+def quick_sort(notas, notasOrig, p, r, numIguais):
     if(r > p):
-        q = particione(fila, p, r)
-        quick_sort(fila, q+1, r)
-        quick_sort(fila, p, q-1)
+        q = particione(notas, p, r)
+        quick_sort(notas, notasOrig, q + 1, r, numIguais)
+        quick_sort(notas, notasOrig, p, q - 1, numIguais)
+        for q in range(len(notas)): 
+          if notas[q] == notasOrig[-q - 1]: numIguais += 1
+    return numIguais
 
-filaOrdenada = filaOriginal.copy()
-for fila in filaOrdenada:
-    quick_sort(fila, 0, len(fila)-1)
-print(filaOriginal)
-print(filaOrdenada)
-
-for i in range(len(filaOriginal)):
-    numIguais = 0
-    for j in range(len(filaOriginal[i])):
-        if(filaOriginal[i][j] == filaOrdenada[i][-j-1]):
-            numIguais += 1
+for i in range(N):
+    M = int(input())
+    notas = [int(nota) for nota in input().split()]
+    numIguais = quick_sort(notas, notas[:], 0, len(notas)-1, 0)
     print(numIguais)
+
